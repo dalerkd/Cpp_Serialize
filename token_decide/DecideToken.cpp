3cf4a4322f38c66702e3203633c19bad866639f6
dalerkd <dalerkd@gmail.com>
2018-07-16 00:56:09 +0800

    完成了全局token处理的一个框架。

--- token_decide/DecideToken.cpp
+++ token_decide/DecideToken.cpp
@@ -10,3 +10,80 @@ CDecideToken::CDecideToken()
 CDecideToken::~CDecideToken()
 {
 }
+
+void CDecideToken::StartDecideToken()
+{
+	//要打印Get行号,字符,以及当前token等信息;
+	try
+	{
+		DecideGlobal();
+	}
+	catch (EXCEP& e)
+	{
+		LogTips("发现一个错误:%s,错误发生在:第%d行,第%d个字符,%d", e.what(), GetLineNumberNow(), GetColumnsNow());
+	}
+
+}
+
+void CDecideToken::DecideGlobal()
+{
+	st_token_value value;
+	while (true)
+	{
+		string typeName = GetToken(value);
+
+		switch (hash_(typeName.c_str()))
+		{
+		case "identifier"_hash://标识符
+		{/*int a*/
+			DecideCreateNewType();
+		}
+		case "keyWord"_hash:
+		{
+			switch (hash_(value.identifier.c_str()))
+			{
+			case ";"_hash:
+			{
+				break;
+			}
+			case "KW_NEW_LINE"_hash://换行
+			{
+				break;
+			}
+			case "struct"_hash:
+			{
+				DecideStruct();
+				break;
+			}
+			default:
+				break;
+			}
+			break;
+		}
+		default:
+		{
+			LogError("未处理的token类型:%s", typeName.c_str());
+			break;
+		}
+		}
+	}
+
+
+}
+/*
+struct iter
+*/
+void CDecideToken::DecideStruct()
+{
+
+}
+
+void CDecideToken::DecideSingleLineComments()
+{
+	throw("");
+}
+
+void CDecideToken::DecideMultiLineComments()
+{
+	throw("");
+}
--- token_decide/DecideToken.h
+++ token_decide/DecideToken.h
@@ -1,4 +1,32 @@
 #pragma once
+
+#include "debug.h"
+#include <string>
+using std::string;
+
+typedef std::uint64_t hash_t;
+constexpr hash_t prime = 0x100000001B3ull;
+constexpr hash_t basis = 0xCBF29CE484222325ull;
+hash_t hash_(char const* str)
+{
+	hash_t ret{ basis };
+	while (*str) {
+		ret ^= *str;
+		ret *= prime;
+		str++;
+	}
+	return ret;
+}
+constexpr hash_t hash_compile_time(char const* str, hash_t last_value = basis)
+{
+	return *str ? hash_compile_time(str + 1, (*str ^ last_value) * prime) : last_value;
+}
+constexpr unsigned long long operator "" _hash(char const* p, size_t)
+{
+	return hash_compile_time(p);
+}
+
+
 /*
 用于处理Token
 分级处理:
@@ -52,5 +80,51 @@ class CDecideToken
 public:
 	CDecideToken();
 	~CDecideToken();
+
+public:
+	struct st_token_value {
+		string identifier;//标识符
+		string keyWord;//关键字
+		/*
+		string 字符串
+		int value;
+		*/
+
+	};
+	/*
+	返回值: 类型名: identifier|keyWord
+	- 内部会处理注释,所以不会返回注释
+	- 换行关键字是:KW_NEW_LINE
+	包括\n或者\r\n
+
+	*/
+	string GetToken(st_token_value& value);
+	
+	size_t GetLineNumberNow(){
+		return m_lineNumberNow;
+	}
+	size_t GetColumnsNow() {
+		return m_columnsNow;
+	}
+	size_t GetLastTokenOffset(){
+		return m_nowTokenOffset;
+	}
+
+private:
+	size_t m_lineNumberNow;//当前处理第几行，从1开始
+	size_t m_columnsNow;//当前处理的第几列，从1开始
+	size_t m_lastTokenOffset;//当前偏移
+	size_t m_nowTokenOffset;//上次偏移
+
+public:
+	void StartDecideToken();
+	//////////////////////////////////////////////
+	//处理不同的token检查与值获取操作
+	void DecideGlobal();
+	void DecideStruct();
+	void DecideSingleLineComments();//单行注释处理
+	void DecideMultiLineComments();//多行注释处理
+	/*int a; 建议最好检查;而不是直接返回*/
+	void DecideCreateNewType();
 };
 
